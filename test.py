1d71eb0c190105345e12949ff5804c335c45b8ec
kiracosten <kira@costen.ca>
2024-09-28 12:33:19 -0400

    added bar to mygarden

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import numpy as np
 
 st.title('This is the home page')
 st.page_link("pages/mycloset.py", label="look at your closet", icon="👚")
-st.page_link("pages/mygarden.py", label="check your points", icon="🌼")
+st.page_link("pages/mygarden.py", label="check your garden", icon="🌼")
 st.page_link("pages/communitygarden.py", label="visit the community garden", icon="🏡")
 st.page_link("pages/outfitideas.py", label="need outfit ideas?", icon="👗")
