0da9720f9c3142bc46890b8af0ff3803680bc234
lobmaia <lobmaia@icloud.com>
2024-09-28 11:52:49 -0400

    changed title

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 
-st.title('This is the home page')
+st.title('Hello')
 st.page_link("pages/mycloset.py", label="look at your closet", icon="👚")
 st.page_link("pages/mygarden.py", label="check your points", icon="🌼")
 st.page_link("pages/communitygarden.py", label="visit the community garden", icon="🏡")
