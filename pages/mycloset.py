aa37b06939fab68bf680de6a14c0731e7071b312
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2024-09-28 13:41:04 -0400

    OMG U CAN UPLOAD IMAGES

--- pages/mycloset.py
+++ pages/mycloset.py
@@ -2,8 +2,13 @@ import streamlit as st
 
 st.title("Here's your closet")
 
-picture = st.camera_input("Take a pic of your clothes")
+st.text("Take a picture of your outfit!") 
 
-if picture:
-    st.image(picture)
+if 'images' not in st.session_state:
+    st.session_state.images = []
 
+uploadedimages = st.file_uploader("Upload an image", type=['png', 'jpg', 'jpeg'], accept_multiple_files=True)
+if uploadedimages: 
+    for file in uploadedimages: 
+        st.session_state.images.append(file)
+        st.image(file, width=500)
