508f52f5de8f9e08479d792539d32d480aa7c0b7
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2024-09-28 21:13:13 -0400

    community garden works

--- pages/community_garden.py
+++ pages/community_garden.py
@@ -1,52 +1,55 @@
 import streamlit as st
 import pandas as pd 
+import webbrowser
 
-st.title("Wanna buy some clothes??!!!")
+st.title("Welcome to the Community Garden!")
+st.write("Here, you can find items that other users are selling")
+
+robert = "community_garden_images/gucci_shirt.png"
+sabrina = "community_garden_images/dress.jpg"
+bob = "community_garden_images/goingoutfit.webp"
+jojo = "community_garden_images/hashtagelsa.jpg"
 
 st.image("community_garden_images/gucci_shirt.png", width=300)
 st.write("Robert Pattison wants to sell his shirt!")
-st.session_state.robert_data = {
-    'Owner': 'Robert Pattison',
-    'Date Purchased': 'September, 2019',
-    'Condition': 'New',
-    'Brand': 'Gucci',
-    'Selling Price': '$48.00',
-    'Contact Info': '911',
-}
-st.write(st.session_state.robert_data)
+if st.button("Show Robert's Shirt Details"):
+    st.write("Owner: Robert Pattison")
+    st.write("Date Purchased: January, 1920")
+    st.write("Condition: New")
+    st.write("Brand: Gucci")
+    st.write("Selling Price: $200.00")
+if st.button("Contact Robert"):
+    webbrowser.open_new_tab("https://en.wikipedia.org/wiki/Robert_Pattinson")
 
 st.image("community_garden_images/dress.jpg", width=300)
 st.write("Sabrina Carpenter wants to sell her dress!")
-st.session_state.sabrina_data = {
-    'Owner': 'Sabrina Carpenter',
-    'Date Purchased': 'January, 1920',
-    'Condition': 'Mild Wear',
-    'Brand': 'Doir',
-    'Selling Price': '$2348.00',
-    'Contact Info': '1800-GOT-STYLE',
-}
-st.write(st.session_state.sabrina_data)
+if st.button("Show Sabrina's Dress Details"):
+    st.write("Owner: Sabrina Carpenter")
+    st.write("Date Purchased: March, 1865")
+    st.write("Condition: Mild Wear")
+    st.write("Brand: Dior")
+    st.write("Selling Price: $128390.05")
+if st.button("Contact Sabrina"):
+    webbrowser.open_new_tab("https://en.wikipedia.org/wiki/Sabrina_Carpenter")
 
 st.image("community_garden_images/goingoutfit.webp", width=300)
 st.write("Bob Ross wants to sell his outfit!")
-st.session_state.bob_data = {
-    'Owner': 'Bob Ross',
-    'Date Purchased': 'March, 1422',
-    'Condition': 'Heavily Used',
-    'Brand': 'Walmart',
-    'Selling Price': '$2.00',
-    'Contact Info': 'bobby@hotmail.com',
-}
-st.write(st.session_state.bob_data)
+if st.button("Show Bob's Outfit Details"):
+    st.write("Owner: Bob Ross")
+    st.write("Date Purchased: June, 3049")
+    st.write("Condition: Heavy Use")
+    st.write("Brand: Bobber's Paints")
+    st.write("Selling Price: $9.65")
+if st.button("Contact Bob"):
+    webbrowser.open_new_tab("https://en.wikipedia.org/wiki/Bob_Ross")
 
 st.image("community_garden_images/hashtagelsa.jpg", width=300)
-st.write("Jojo Siwa wants to sell her dress!")
-st.session_state.jojo_data = {
-    'Owner': 'Jojo Swia',
-    'Date Purchased': 'December, 2023',
-    'Condition': 'New',
-    'Brand': 'Jojo Bows',
-    'Selling Price': '$63008.00',
-    'Contact Info': 'jojo@jojo.com',
-}
-st.write(st.session_state.jojo_data)
+st.write("Jojo Siwa wants to sell her outfit!")
+if st.button("Show Jojo's Outfit Details"):
+    st.write("Owner: Jojo Siwa")
+    st.write("Date Purchased: April, 2024")
+    st.write("Condition: New")
+    st.write("Brand: Jojo Bows")
+    st.write("Selling Price: $0.50")
+if st.button("Contact Jojo"):
+    webbrowser.open_new_tab("https://en.wikipedia.org/wiki/JoJo_Siwa")
