bd3ddd4c29d3454877b83ce1024cafd840a91600
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2024-09-28 21:20:42 -0400

    sewstainability

--- home_page.py
+++ home_page.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 
-st.title('Hi')
+st.title('Welcome to Sewstainability!')
+st.write('Our mission is to promote green sewlutions for overconsumption in clothing. We aim to help you reduce your carbon footprint by providing you with the tools to make sustainable choices in your wardrobe.')
 st.page_link("pages/my_closet.py", label="look at your closet", icon="👚")
 st.page_link("pages/my_garden.py", label="check your garden", icon="🌼")
 st.page_link("pages/community_garden.py", label="visit the community garden", icon="🏡")
