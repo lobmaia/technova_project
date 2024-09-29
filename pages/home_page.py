62b9fdfe6b58631bc6a5bc30bbdeeabe7f1c8fca
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2024-09-29 08:02:07 -0400

    deleted link to outfit ideas on home page

--- pages/home_page.py
+++ pages/home_page.py
@@ -15,4 +15,3 @@ with cent_co:
 st.page_link("pages/my_closet.py", label="look at your closet", icon="👚")
 st.page_link("pages/my_garden.py", label="check your garden", icon="🌼")
 st.page_link("pages/community_garden.py", label="visit the community garden", icon="🏡")
-st.page_link("pages/outfit_ideas.py", label="need outfit ideas?", icon="👗")
